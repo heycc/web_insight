c5e12e72c00d790b4760842facbcbd4ec080c03b
heycc <cc.changchun@gmail.com>
2025-03-07 20:47:20 +0800

    clean old version

--- scripts/marked-check.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("Marked.js loaded:", typeof marked !== 'undefined');
-  if (typeof marked !== 'undefined') {
-    console.log("Marked.js version:", marked.version || "unknown");
-  } else {
-    console.error("Marked.js not loaded properly!");
-  }
-});
